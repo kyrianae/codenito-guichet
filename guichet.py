5cd6b7ebddccbf248d69e31a75f908e001412e39
kyrianae <kyrianae@gmail.com>
2023-04-05 18:27:28 +0200

    guichet app name

--- guichet.py
+++ guichet.py
@@ -2,11 +2,11 @@
 from flask import Flask, json, request, Response
 import random
 
-api = Flask("guichet")
+app = Flask("guichet")
 
-@api.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def hello():
     return "Hello World\n"
 
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8081)
