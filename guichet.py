b9263a3a583b6f81c32d82f15437c8f973025559
kyrianae <kyrianae@gmail.com>
2023-04-05 20:11:36 +0200

    added date

--- guichet.py
+++ guichet.py
@@ -3,24 +3,30 @@ from flask import Flask, json, request, Response
 import random
 import os
 import requests
+import datetime
 
 app = Flask("guichet")
 pizzaiolo="http://127.0.0.1:8082"
+
+def str_now():
+    return str(datetime.datetime.now())
 
 if "PIZZAIOLO" in os.environ:
     pizzaiolo=os.environ["PIZZAIOLO"]
     
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Guicher says: Hello World\n"
+    return str_now()+"\t"+"Guicher says: Hello World\n"
 
 @app.route('/pizza', methods=['GET'])
 def pizza():
-    print ("Guichet\customer asked pizza")
-    x = requests.get(pizzaiolo+"/make_pizza")
+    print (str_now()+"\t"+"Guichet\customer asked pizza")
+    url=pizzaiolo+"/make_pizza"
+    print (str_now()+"\t"+"Guicher calls: "+url)
+    x = requests.get(url)
     print (x.text)
-    print ("Guichet\tPizza delivered")
-    return x.text+"\nGuichet\tPizza delivered\n"
+    print (str_now()+"\t"+"Guichet\t\tPizza delivered")
+    return x.text+"\n"+str_now()+"\t"+"Guichet\t\tPizza delivered\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
